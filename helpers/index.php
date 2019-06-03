cdae3bcbb3ea3cf2afece4e7eb6edbc0e3a2d3e5
bTFFH <evro.dolor@mail.ru>
2019-06-03 12:09:45 +0300

    Registration improvements

--- helpers/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['username'])) {
-    $_SESSION['errno'] = 403;
-    $_SESSION['error'] = "Access restricted";
-    header("Location: error.php", true, 303);
-}
-else header("Location: ../", true, 303);
-?>
--- helpers/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['username']);
-unset($_SESSION['name']);
-header("Location: /IndZ/index.php");
-?>
